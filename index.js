e03737328512b306f64864072da1f36e48670b66
Sejan Mahmud <sejan157218@gmail.com>
2022-01-20 17:59:28 +0600

    add data

---
+++ index.js
@@ -0,0 +1,146 @@
+[
+    {
+      "id": "fgdfgdfg1",
+      "title": "Front End Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg2",
+      "title": "React JS Developer",
+      "shift": "Day",
+      "vacancy": 5,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg3",
+      "title": "Front End Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg4",
+      "title": "Javascript Developer",
+      "shift": "Day",
+      "vacancy": 4,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg5",
+      "title": "Front End Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg6",
+      "title": "Back End Developer",
+      "shift": "Day",
+      "vacancy": 4,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdf7",
+      "title": "Front End Developer",
+      "shift": "Day",
+      "vacancy": 3,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg8",
+      "title": "Full Stack Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg9",
+      "title": "MERN Stack Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg10",
+      "title": "Front End Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg11",
+      "title": "Full Stack Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg12",
+      "title": "Front End Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg13",
+      "title": "MERN Stack Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg14",
+      "title": "React JS Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg",
+      "title": "Front End Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg15",
+      "title": "React JS Developer",
+      "shift": "Day",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg16",
+      "title": "Front End Developer",
+      "shift": "Night",
+      "vacancy": 2,
+      "salary": 20000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    },
+    {
+      "id": "fgdfgdfg17",
+      "title": "Back End Developer",
+      "shift": "Day",
+      "vacancy": 5,
+      "salary": 25000,
+      "desc": "It's a very good quality job. If you try to apply for this Job circular then you can also get a good future. It's a full-time job. You can know how many people will be appointed on their different posts by seeing the below notice picture. You can also apply if you are interested and if you have fulfilled qualifications as their demand. It Job is recruiting some young and talented people for the following available positions."
+    }
+  ]
